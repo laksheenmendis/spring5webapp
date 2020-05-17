4b51d1519e5335273e8e00e0c52ac495084b1cb4
laksheenmendis <cresclux@yahoo.com>
2020-05-17 01:52:30 -0600

    Changing the server port

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -8,5 +8,11 @@ public class Spring5webappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
+
+		// Below code segment could be used to change the starting port from 8080 to another custom port
+		// otherwise, set the server.port=9090 in application.properties
+		/*SpringApplication application = new SpringApplication(Spring5webappApplication.class);
+		application.setDefaultProperties(Collections.singletonMap("server.port", "9090"));
+		application.run(args);*/
 	}
 }
